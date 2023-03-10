ab06e964342cdb6816f3e032c931644e8b5683ac
asmitaj08 <asmita.jha82@gmail.com>
2023-03-09 18:09:56 -0800

    auto run for particular product and particular vendor

--- _TEW731BRV1_FW102B05.bin.extracted/squashfs-root/bin/l2tp_demand.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "l2tp demand on">/dev/console
-killall pppd
-echo "c client">/var/run/l2tp-control
-
--- _TEW731BRV1_FW102B05.bin.extracted/squashfs-root/bin/l2tp_tunnel_connect.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "l2tp tunnel up">/dev/console
-app_sync 1029 0 "act=up"
